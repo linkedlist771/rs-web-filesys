03fb7aed7b52a8ba535e4b89b907099e062442f2
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-08-18 20:05:43 +0800

    Update constants.ts

--- front/src/config/constants.ts
+++ front/src/config/constants.ts
@@ -1,3 +1,3 @@
 // http://localhost:8080/api/v1
-export const BACKEND_URL = 'http://localhost:8080'
+export const BACKEND_URL = 'http://101.132.169.133:1145'
 export const BASE_URL = `${BACKEND_URL}/api/v1` 
